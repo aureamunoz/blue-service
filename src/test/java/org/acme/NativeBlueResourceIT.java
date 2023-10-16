e6022d2d67c4f3917c58b644fcd59c59298b22c5
Auri Munoz <amunozhe@redhat.com>
2023-10-16 22:24:02 +0200

    upgrade to Quarkus 3.x and align code accordingly

--- src/test/java/org/acme/NativeBlueResourceIT.java
+++ src/test/java/org/acme/NativeBlueResourceIT.java
@@ -1,8 +1,8 @@
 package org.acme;
 
-import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@NativeImageTest
+@QuarkusIntegrationTest
 public class NativeBlueResourceIT extends BlueResourceTest {
 
     // Execute the same tests but in native mode.
